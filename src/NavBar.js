11ebcd849ac743454ad942ce253ab8e1c92c5561
Cooper Michael <coopermichael@Coopers-MacBook-Pro.local>
2019-10-14 13:10:13 -0700

    Added routes, homepage, and footer

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,16 +4,24 @@ import 'rc-slider/assets/index.css';
 import './NavBar.css';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import { IconButton } from '@material-ui/core';
+import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { format: 'hex' };
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { format: 'hex', open: false };
+        this.handleFormatChange = this.handleFormatChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
     }
-    handleChange(e){
-        this.setState({format:e.target.value});
+    handleFormatChange(e){
+        this.setState({format: e.target.value, open: true });
         this.props.handleChange(e.target.value);
+    }
+    closeSnackbar() {
+        this.setState({open: false});
     }
     render() {
         const {level, changeLevel} = this.props;
@@ -21,7 +29,7 @@ class NavBar extends Component {
         return (
             <header className="NavBar">
                 <div className="logo">
-                    <a href="#">reactcolorpicker</a>
+                    <Link to='/'>reactcolorpicker</Link>
                 </div>
                 <div className="slider-container">
                     <span>Level: {level}</span>
@@ -36,12 +44,27 @@ class NavBar extends Component {
                     </div>
                 </div>
                 <div className="select-container">
-                    <Select value={format} onChange={this.handleChange}>
+                    <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value="hex">HEX - #ffffff</MenuItem>
                         <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
                         <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
                     </Select>
                 </div>
+                <Snackbar 
+                    anchorOrigin={{vertical: "bottom", horizontal: "left"}}
+                    open={this.state.open}
+                    autoHideDuration={3000}
+                    message={<span id='message-id'>Color format changed to {format.toUpperCase()}</span>}
+                    ContentProps={{
+                        "aria-describedby": "message-id"
+                    }}
+                    onClose={this.closeSnackbar}
+                    action={[
+                        <IconButton onClick={this.closeSnackbar} color='inherit' key='close' aria-label='close'>
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+                />
             </header>
         )
     }
--- src/Palette.js
+++ src/Palette.js
@@ -17,10 +17,10 @@ class Palette extends React.Component {
         this.setState({ format: val })
     }
     render () {
-        const { colors } = this.props.palette
+        const { colors, paletteName, emoji } = this.props.palette
         const { level, format } = this.state
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color[format]} name={color.name}/>
+            <ColorBox background={color[format]} key={color.id} name={color.name}/>
         ));
         return(
             <div className="Palette">
@@ -28,7 +28,12 @@ class Palette extends React.Component {
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
-                {/* footer */}
+                <footer className="Palette-footer">
+                    <div>
+                        {paletteName}
+                        <span className="emoji">{emoji}</span>
+                    </div>
+                </footer>
             </div>
         )
     }
